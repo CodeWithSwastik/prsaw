6f672839943cceeaca622216e019cb5ca7fa449b
FalseDev <59121948+FalseDev@users.noreply.github.com>
2021-05-12 16:01:09 +0530

    Added experimental support for V3

--- prsaw/PRSAW.py
+++ prsaw/PRSAW.py
@@ -1,25 +1,10 @@
+from enum import Enum
+
 import httpx
-from apiclient import APIClient, APIRouter, Get, endpoint
+from apiclient import APIClient, Get, endpoint
 
 
-class RandomStuff(APIClient):
-    """
-    A Wrapper for the Random Stuff API.
-
-    Example Usage:
-
-        rs = RandomStuff(api_key = "Your API Key")
-        joke = rs.get_joke()
-        print(joke)
-        rs.close()
-
-    Example async usage:
-        rs = RandomStuff(async_mode=True, api_key="Your API Key")
-        joke = await rs.get_joke()
-        print(joke)
-        await rs.close()
-    """
-
+class BaseClient(APIClient):
     _joke_types = ("dev", "spooky", "pun", "any")
     _image_types = (
         "aww",
@@ -34,17 +19,6 @@ class RandomStuff(APIClient):
         "facepalm",
         "any",
     )
-
-    def __init__(self, *, async_mode=False, api_key: str = None):
-        session = httpx.AsyncClient() if async_mode else httpx.Client()
-        self.base_url = "https://api.pgamerx.com"
-        if api_key:
-            session.params["api_key"] = api_key
-        else:
-            self.base_url += "/demo"
-            
-        self.api_key = api_key
-        super().__init__(session=session)
 
     def _pre_init(self):
         # Convert to json before returning
@@ -66,6 +40,42 @@ class RandomStuff(APIClient):
 
         return Get("/image/" + _type)
 
+    def close(self):
+        session = self.session
+        return (
+            session.close() if isinstance(session, httpx.Client) else session.aclose()
+        )
+
+
+class RandomStuff(BaseClient):
+    """
+    A Wrapper for the Random Stuff API.
+
+    Example Usage:
+
+        rs = RandomStuff(api_key = "Your API Key")
+        joke = rs.get_joke()
+        print(joke)
+        rs.close()
+
+    Example async usage:
+        rs = RandomStuff(async_mode=True, api_key="Your API Key")
+        joke = await rs.get_joke()
+        print(joke)
+        await rs.close()
+    """
+
+    def __init__(self, *, async_mode=False, api_key: str = None):
+        session = httpx.AsyncClient() if async_mode else httpx.Client()
+        self.base_url = "https://api.pgamerx.com"
+        if api_key:
+            session.params["api_key"] = api_key
+        else:
+            self.base_url += "/demo"
+
+        self.api_key = api_key
+        super().__init__(session=session)
+
     @endpoint
     def get_ai_response(self, msg: str, *, lang="en") -> str:
         return Get("/ai/response", params={"message": msg, "language": lang})
@@ -75,5 +85,64 @@ class RandomStuff(APIClient):
 
     _post_get_ai_response = _post_get_image
 
-    def close(self):
-        return self.session.close()
+
+class ApiPlan(Enum):
+    PRO = "pro"
+    ULTRA = "ultra"
+    BIZ = "biz"
+    MEGA = "mega"
+
+
+class RandomStuffV3(BaseClient):
+    def __init__(
+        self,
+        api_key: str,
+        *,
+        async_mode=False,
+        plan: ApiPlan = None,
+        dev_name: str = None,
+        bot_name: str = None,
+        ai_language: str = None,
+    ):
+        session = httpx.AsyncClient() if async_mode else httpx.Client()
+
+        # URL construction
+        self.base_url = "https://api.pgamerx.com/v3"
+        if plan:
+            self.base_url += "/" + plan.value
+
+        # Authorization
+        if api_key:
+            session.headers["x-api-key"] = api_key
+
+        self.dev_name = dev_name
+        self.bot_name = bot_name
+        self.ai_language = ai_language
+
+        self.api_key = api_key
+        super().__init__(session=session)
+
+    @endpoint
+    def get_ai_response(
+        self,
+        message: str,
+        *,
+        unique_id: str = None,
+        dev_name: str = None,
+        bot_name: str = None,
+        language: str = None,
+    ):
+        params = {"message": message}
+        if unique_id:
+            params["unique_id"] = unique_id
+
+        if dev_name or self.dev_name:
+            params["dev_name"] = dev_name or self.dev_name
+
+        if language or self.ai_language:
+            params["language"] = dev_name or self.ai_language
+
+        if bot_name or self.bot_name:
+            params["bot_name"] = bot_name or self.bot_name
+
+        return Get("/ai/response", params=params)
